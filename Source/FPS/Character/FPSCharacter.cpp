2ed786e8867afb96c47f1ff9cb0e3f83f07d6f8b
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2026-07-06 09:59:22 -0300

    Initial commit

---
+++ Source/FPS/Character/FPSCharacter.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+AFPSCharacter::AFPSCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 0.0f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 15.0f;
+	SpringArm->bUsePawnControlRotation = true;
+	
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCamera->SetupAttachment(SpringArm);
+	FirstPersonCamera->bUsePawnControlRotation = false;
+	
+	FirstPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	FirstPersonMesh->SetupAttachment(FirstPersonCamera);
+	FirstPersonMesh->bOnlyOwnerSee = true;
+	FirstPersonMesh->bOwnerNoSee = false;
+	FirstPersonMesh->bCastDynamicShadow = false;
+	FirstPersonMesh->bReceivesDecals = false;
+	FirstPersonMesh->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickMontagesWhenNotRendered;
+	FirstPersonMesh->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+	
+	auto* ThirdPersonMesh = GetMesh();
+	ThirdPersonMesh->bOnlyOwnerSee = false;
+	ThirdPersonMesh->bOwnerNoSee = true;
+	ThirdPersonMesh->bReceivesDecals = false;
+	
+}
+
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/FPS/Character/FPSCharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class FPS_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFPSCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USkeletalMeshComponent> FirstPersonMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USpringArmComponent> SpringArm;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCameraComponent> FirstPersonCamera;
+	
+protected:
+	virtual void BeginPlay() override;
+};
