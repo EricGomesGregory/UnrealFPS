57a35d8d607c1cd4478bd8c47e92068eef305d3c
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2026-07-06 13:43:22 -0300

    Add JetBrains Rider files

---
+++ Source/FPS/Game/FPSGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
