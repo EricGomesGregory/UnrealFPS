0d31f9a30c62fe946e49180b733a23fbc6b9cdfd
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2026-07-06 16:41:11 -0300

    Setup weapon classes

---
+++ Source/FPS/Weapon/Weapon.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true;
+	
+	FirstPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	FirstPersonMesh->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	FirstPersonMesh->bReceivesDecals = false;
+	FirstPersonMesh->CastShadow = false;
+	FirstPersonMesh->SetHiddenInGame(true);
+	SetRootComponent(FirstPersonMesh);
+	
+	ThirdPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ThirdPersonMesh"));
+	ThirdPersonMesh->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	ThirdPersonMesh->bReceivesDecals = false;
+	ThirdPersonMesh->SetupAttachment(FirstPersonMesh);
+	ThirdPersonMesh->SetHiddenInGame(true);
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/FPS/Weapon/Weapon.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class FPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+
+	UFUNCTION(BlueprintCallable, Category="FPS|Weapon")
+	USkeletalMeshComponent* GetFirstPersonMesh() const { return FirstPersonMesh; }
+	
+	UFUNCTION(BlueprintCallable, Category="FPS|Weapon")
+	USkeletalMeshComponent* GetThirdPersonMesh() const { return ThirdPersonMesh; }
+	
+	
+protected:
+	/** */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USkeletalMeshComponent> FirstPersonMesh;
+	
+	/** */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USkeletalMeshComponent> ThirdPersonMesh;
+	
+protected:
+	virtual void BeginPlay() override;
+
+};
