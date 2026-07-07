f06abc87a5d8635e4005cb01c1a4cf211250e644
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2026-07-07 13:27:08 -0300

    Add Player interface

---
+++ Source/FPS/Interfaces/PlayerInterface.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PlayerInterface.generated.h"
+
+struct FGameplayTag;
+
+
+UINTERFACE()
+class UPlayerInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class FPS_API IPlayerInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	FName GetWeaponAttachPointSocketName(const FGameplayTag& WeaponTyeTag) const;
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	USkeletalMeshComponent* GetFirstPersonSkeletalMeshComponent() const;
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	USkeletalMeshComponent* GetThirdPersonSkeletalMeshComponent() const;
+};
